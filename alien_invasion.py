72225416bf749a4559abd9324df751b6ce682f7d
Anthony <anthonyarmbruster@gmail.com>
2019-10-07 19:33:39 -0400

    Refactor key processing, added Q to 'quit'

--- alien_invasion.py
+++ alien_invasion.py
@@ -32,19 +32,25 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    # Move ship right.
-                    self.ship.moving_right = True
-                if event.key == pygame.K_LEFT:
-                    # Move ship left.
-                    self.ship.moving_left = True
+                self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    # Move ship right.
-                    self.ship.moving_right = False
-                if event.key == pygame.K_LEFT:
-                    # Move ship left.
-                    self.ship.moving_left = False
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """Respond to key presses"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        if event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        if event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        """Respond to key releases."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        if event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
