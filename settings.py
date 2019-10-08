8d508b864da9f2b8dcd6b07b352382b5a0c5ef5a
Anthony <anthonyarmbruster@gmail.com>
2019-10-07 20:11:57 -0400

    added fullscreen mode

--- settings.py
+++ settings.py
@@ -4,6 +4,7 @@ class Settings:
     def __init__(self):
         """Initialize te game's settings."""
         # Screen settings
+        self.full_screen = True
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)  # light grey
@@ -11,4 +12,3 @@ class Settings:
         # ship settings
         self.ship_speed = 1.5
 
-        
