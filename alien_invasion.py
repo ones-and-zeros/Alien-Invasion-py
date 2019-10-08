e7c9fc843759cddce2854a5f6359677ce71c62e7
Anthony <anthonyarmbruster@gmail.com>
2019-10-07 22:05:02 -0400

    use defined resolution in fullscreen

--- alien_invasion.py
+++ alien_invasion.py
@@ -15,9 +15,12 @@ class AlienInvasion:
         self.settings = Settings()
 
         if self.settings.full_screen:
-            self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-            self.settings.screen_width = self.screen.get_rect().width
-            self.settings.screen_height = self.screen.get_rect().height
+            self.screen = pygame.display.set_mode(
+                (self.settings.screen_width, self.settings.screen_height),
+                pygame.FULLSCREEN)
+            # self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+            # self.settings.screen_width = self.screen.get_rect().width
+            # self.settings.screen_height = self.screen.get_rect().height
         else:
             self.screen = pygame.display.set_mode(
                 (self.settings.screen_width, self.settings.screen_height))
