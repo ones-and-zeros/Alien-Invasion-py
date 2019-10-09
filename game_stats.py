c7bb5dd4389ed292fd7208ae7216ccfe6cadc029
Anthony <anthonyarmbruster@gmail.com>
2019-10-08 21:24:59 -0400

    end game after 3 ships are lost

--- game_stats.py
+++ game_stats.py
@@ -9,3 +9,5 @@ class GameStats:
     def reset_stats(self):
         """Initialize statisctics that can change during game"""
         self.ships_left = self.settings.ship_limit
+        # Start game in active state 
+        self.game_active = True
